e73025750b4e1d2486077d2e0a0f8fa40b8b2799
aliickrr <alickrr11@gmail.com>
2019-08-07 09:55:02 +0300

    database connection

---
+++ Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog_Project.Models
+{
+    public class Blog
+    {
+        public Blog()
+        {
+        }
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public string Content { get; set; }
+        public int LikesCount { get; set; }
+        public int PostId { get; set; }
+    }
+
+    
+}
---
+++ Models/ModelConfigurations/BlogConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog_Project.Models.ModelConfigurations
+{
+    public class BlogConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Content).HasMaxLength(500);
+
+            builder.Property(prop => prop.LikesCount);
+
+            builder.Property(prop => prop.PostId);
+
+
+        }
+    }
+}
