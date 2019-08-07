e1cb1beef4260ffda97da6c0bdb6eff96075cb26
aliickrr <alickrr11@gmail.com>
2019-08-07 11:55:20 +0300

    Comment table added

--- Models/ModelConfigurations/CommentConfiguration.cs
+++ Models/ModelConfigurations/CommentConfiguration.cs
@@ -6,8 +6,24 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Blog_Project.Models.ModelConfigurations
 {
-    public class CommentConfiguration
+    public class CommentConfiguration: IEntityTypeConfiguration<Comment>
     {
 
+
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Content).HasMaxLength(500);
+
+            builder.Property(prop => prop.LikeCount);
+
+            builder.Property(prop => prop.PostId);
+
+            builder.Property(prop => prop.Date);
+
+
+
+        }
     }
 }
